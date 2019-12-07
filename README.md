b11b37d2269d3445b7c4ec88aa20022e8535611d
Owen Chanthala <owenchanthala@gmail.com>
2019-12-07 14:59:33 -0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # 3-Password-Generator
+
+## Table of contents
+* [Assignment Info](#assignment-info)
+* [Technologies](#technologies)
+* [Files](#files)
+* [Key Notes](#key-notes)
+
+## Assignment info
+In this assignment, I myself got familiar with creating a javascript file. 
+A file separate from the html, which we usually add at the bottom of the
+html body. Instead of having one long html file, it will be separated between 
+the two. Within the javascript file,the user will click generate password, 
+which calls a function. Then there are prompts and confirm messages for the 
+user. To pick different criteria for their password. Using arrays, if/else 
+conditions, for loops and while loops to analyze the user criteria and create 
+a password. The Password will be displayed in the textbox for the user and when
+the copy button is clicked. That will call a function to copy the generated password
+to the user's clipboard.
+
+	
+## Technologies
+Project is created with:
+* Bootstrap v4.4
+* Visual Studios Code (HTML/CSS/JS)
+* Google
+	
+## Files
+Main files for this assignment include:
+
+* Assignment contains:
+* 1 html file:
+
+ ![3-Password-Generator](password.html) 
+
+* 1 CSS file:
+
+ ![3-Password-Generator](style.css)
+
+* 1 Javascript file:
+
+![3-Password-Generator](script.js)
+
+## Key Notes
+* Make sure to include the bootstrap link:
+
+```
+$ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+```
+* 2 Main functions within the 2 buttons. Which is the brains of the assignment.
---
+++ script.js
@@ -0,0 +1,277 @@
+
+var validc1 = false;
+var good = false;
+var password = [];
+var type;
+var entry;
+var passGen;
+var input;
+var combo1 = ['sc', 'nc', 'lc', 'uc',];
+var combo2 = ['nc', 'lc', 'uc',];
+var combo3 = ['sc', 'lc', 'uc',];
+var combo4 = ['sc', 'nc', 'uc',];
+var combo5 = ['sc', 'nc', 'lc',];
+var combo6 = ['sc', 'nc',];
+var combo7 = ['sc', 'lc',];
+var combo8 = ['sc', 'uc',];
+var combo9 = ['nc', 'lc',];
+var combo10 = ['nc', 'uc',];
+var combo11 = ['lc', 'uc',];
+var sc = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+var nc = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+function generate() {
+    var c1 = prompt("Enter how long you would like your password to be. (8 - 128)");
+    password.length = c1;
+
+    while (!validc1) {
+        if (c1 < 8 || c1 > 128) {
+            c1 = prompt("Invalid entry! Must be between 8 and 128!");
+        }
+        else {
+            validc1 = true;
+            password.length = c1;
+        }
+    }
+
+    var c2 = confirm("Do you want Special Characters? (!@#$%)");
+    var c3 = confirm("Do you want Numeric Characters? (12345)");
+    var c4 = confirm("Do you want Lowercase Characters? (abcde)");
+    var c5 = confirm("Do you want Uppercase Characters? (FGHIJ)");
+
+    if (c2 === true || c3 === true || c4 === true || c5 === true) {
+        good = true;
+    }
+    else {
+        good = false;
+    }
+
+    while (!good) {
+        alert("Must confirm at least 1 criteria!")
+        var c2 = confirm("Do you want Special Characters? (!@#$%)");
+        var c3 = confirm("Do you want Numeric Characters? (12345)");
+        var c4 = confirm("Do you want Lowercase Characters? (abcde)");
+        var c5 = confirm("Do you want Uppercase Characters? (FGHIJ)");
+
+        if (c2 === true || c3 === true || c4 === true || c5 === true) {
+            good = true;
+        }
+        else {
+            good = false;
+        }
+    }
+
+    if (c2 === true && c3 === true && c4 === true && c5 === true) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo1[Math.floor(Math.random() * 4)];
+            if (type === 'sc') {
+                entry = sc[Math.floor(Math.random() * 32)];
+                password[x] = entry;
+            }
+            else if (type === 'nc') {
+                entry = nc[Math.floor(Math.random() * 10)];
+                password[x] = entry;
+            }
+            else if (type === 'lc') {
+                entry = lc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+            else {
+                entry = uc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === false && c3 === true && c4 === true && c5 === true) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo2[Math.floor(Math.random() * 3)];
+            if (type === 'nc') {
+                entry = nc[Math.floor(Math.random() * 10)];
+                password[x] = entry;
+            }
+            else if (type === 'lc') {
+                entry = lc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+            else {
+                entry = uc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === true && c3 === false && c4 === true && c5 === true) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo3[Math.floor(Math.random() * 3)];
+            if (type === 'sc') {
+                entry = sc[Math.floor(Math.random() * 32)];
+                password[x] = entry;
+            }
+            else if (type === 'lc') {
+                entry = lc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+            else {
+                entry = uc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === true && c3 === true && c4 === false && c5 === true) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo4[Math.floor(Math.random() * 3)];
+            if (type === 'sc') {
+                entry = sc[Math.floor(Math.random() * 32)];
+                password[x] = entry;
+            }
+            else if (type === 'nc') {
+                entry = nc[Math.floor(Math.random() * 10)];
+                password[x] = entry;
+            }
+            else {
+                entry = uc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === true && c3 === true && c4 === true && c5 === false) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo5[Math.floor(Math.random() * 3)];
+            if (type === 'sc') {
+                entry = sc[Math.floor(Math.random() * 32)];
+                password[x] = entry;
+            }
+            else if (type === 'nc') {
+                entry = nc[Math.floor(Math.random() * 10)];
+                password[x] = entry;
+            }
+            else {
+                entry = lc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === true && c3 === true && c4 === false && c5 === false) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo6[Math.floor(Math.random() * 2)];
+            if (type === 'sc') {
+                entry = sc[Math.floor(Math.random() * 32)];
+                password[x] = entry;
+            }
+            else {
+                entry = nc[Math.floor(Math.random() * 10)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === true && c3 === false && c4 === true && c5 === false) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo7[Math.floor(Math.random() * 2)];
+            if (type === 'sc') {
+                entry = sc[Math.floor(Math.random() * 32)];
+                password[x] = entry;
+            }
+            else {
+                entry = lc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === true && c3 === false && c4 === false && c5 === true) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo8[Math.floor(Math.random() * 2)];
+            if (type === 'sc') {
+                entry = sc[Math.floor(Math.random() * 32)];
+                password[x] = entry;
+            }
+            else {
+                entry = uc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === false && c3 === true && c4 === true && c5 === false) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo9[Math.floor(Math.random() * 2)];
+            if (type === 'nc') {
+                entry = nc[Math.floor(Math.random() * 10)];
+                password[x] = entry;
+            }
+            else {
+                entry = lc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === false && c3 === true && c4 === false && c5 === true) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo10[Math.floor(Math.random() * 2)];
+            if (type === 'nc') {
+                entry = nc[Math.floor(Math.random() * 10)];
+                password[x] = entry;
+            }
+            else {
+                entry = uc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === false && c3 === false && c4 === true && c5 === true) {
+        for (var x = 0; x < password.length; x++) {
+            type = combo11[Math.floor(Math.random() * 2)];
+            if (type === 'lc') {
+                entry = lc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+            else {
+                entry = uc[Math.floor(Math.random() * 26)];
+                password[x] = entry;
+            }
+        }
+    }
+    else if (c2 === true && c3 === false && c4 === false && c5 === false) {
+        for (var x = 0; x < password.length; x++) {
+            entry = sc[Math.floor(Math.random() * 32)];
+            password[x] = entry;
+        }
+    }
+    else if (c2 === false && c3 === true && c4 === false && c5 === false) {
+        for (var x = 0; x < password.length; x++) {
+            entry = nc[Math.floor(Math.random() * 10)];
+            password[x] = entry;
+        }
+    }
+    else if (c2 === false && c3 === false && c4 === true && c5 === false) {
+        for (var x = 0; x < password.length; x++) {
+            entry = lc[Math.floor(Math.random() * 26)];
+            password[x] = entry;
+        }
+    }
+    else {
+        for (var x = 0; x < password.length; x++) {
+            entry = uc[Math.floor(Math.random() * 26)];
+            password[x] = entry;
+        }
+    }
+
+    console.log(password.join(''));
+    input = document.getElementById("password");
+    input.value = password.join('');
+    console.log(input.value);
+}
+
+function copy() {
+    var copyPassword = document.getElementById("password");
+    copyToClipboard(copyPassword.value);
+    alert("Copied new password: " + copyPassword.value);
+}
+
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
